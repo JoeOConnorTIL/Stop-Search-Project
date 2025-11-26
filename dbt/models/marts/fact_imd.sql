c663e5d822731486ede2a3bcc063e20266cbed5f
Joe O'Connor <jpoc1g11@gmail.com>
2025-11-26 15:12:42 +0000

    Updates to DS

--- dbt/models/marts/fact_imd.sql
+++ dbt/models/marts/fact_imd.sql
@@ -6,11 +6,7 @@ WITH imd AS (
 )
 
 SELECT 
-i.lsoa_code,
-d.dep_id,
-i.rank,
-i.decile,
-i.score
-FROM imd i
-LEFT JOIN imd_dim d
-ON LEFT(i.dep_index, 1) = d.dep_id
+lsoa21cd AS lsoa_Code,
+imd_rank,
+imd_decile
+FROM imd
